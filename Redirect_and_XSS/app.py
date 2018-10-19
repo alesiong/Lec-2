7386fbb738850e93605372eafc1bbe2f1da6829a
Brando Zhang <116010308@link.cuhk.edu.cn>
2018-10-19 17:02:44 +0800

    Add: Task 1
    This is the Task 1 for you to review.

---
+++ Redirect_and_XSS/app.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Task 1:
+    a.  Visit http://127.0.0.1/, and then visit http://127.0.0.1/set/<type your name here> to set a cookie.
+        When you revisit http://127.0.0.1/, the website still remembers your name.
+    b.  Complete hi() so that when user visits http://127.0.0.1/hi, it will be redirect to http://127.0.0.1/hello.
+    c.  Implement XSS via URL. (It may fails on Safari and Chrome, I will use Firefox to show)
+    d.  Fix this XSS loophole in hello().
+    optional:   Design your own set_cookie and corresponding view functions by referring the following programs.
+"""
+from flask import Flask, request, make_response, redirect, url_for
+
+app = Flask(__name__)
+
+
+# set cookie
+@app.route('/set/<name>')
+def set_cookie(name):
+    response = make_response(redirect(url_for('hello')))
+    response.set_cookie('name', name)
+    return response
+
+
+"""
+Incomplete programs start from here
+"""
+
+
+@app.route('/')
+@app.route('/hello')
+def hello():
+    name = request.args.get('name')
+    if name is None:
+        name = request.cookies.get('name', 'Human')
+    response = '<h1>Hello, %s!</h1>' % name
+    return response
+
+
+def hi():
+    pass
